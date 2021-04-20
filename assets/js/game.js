cfeae76f76bd45f1cffa3ba0297f33cfb82aab82
ktodoran <ktodoran@gmail.com>
2021-04-19 22:08:51 -0400

    Modified game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,6 @@ function fight() {
   window.alert("The fight has begun!");
 }
 
-window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
 
 fight();
